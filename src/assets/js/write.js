9a5f0e04ee22340df3eca2c0e998d9050075dc10
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-08-17 21:42:35 +0900

    feat : wirte HTML CSS

---
+++ src/assets/js/write.js
@@ -0,0 +1,35 @@
+const $textarea = document.querySelector('.post-contents');
+const $imageInput = document.querySelector('.image-input')
+const $postImage = document.querySelector('.post-image')
+const $backBtn = document.querySelector('.post-back')
+
+// 줄바꿈 시 자동으로 height 늘어나는 함수
+$textarea.oninput = (event) => {
+    const $target = event.target;
+    $target.style.height = 0;
+    $target.style.height = $target.scrollHeight + 'px';
+};
+
+const previewImage = (event) => {
+    const file = event.target.files[0];
+
+    if (file.size > 250000){
+        alert('파일크기는 2.5MB 이내로 가능합니다.')
+        event.target.value = ''
+    } else{
+        let reader = new FileReader();
+
+        reader.onload = function (event) {
+            $postImage.setAttribute("src", event.target.result);
+        };
+        reader.readAsDataURL(file);
+    }
+    
+};
+
+const backFunc = () => {
+    window.history.back();
+}
+
+$imageInput.addEventListener("change", previewImage);
+$backBtn.addEventListener('click', backFunc)
