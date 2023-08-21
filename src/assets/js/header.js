01801d2202c6066aaacf7d20a5e212ddd83a60d6
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-08-21 18:43:32 +0900

    feat : header.js 추가

---
+++ src/assets/js/header.js
@@ -0,0 +1,38 @@
+const $logout_btn = document.querySelector('.logout_btn')
+
+// 로그인이 되있는지 확인
+const is_logined = () => {
+
+    if (localStorage.getItem('user')) {
+        const is_logined = document.querySelectorAll('.is_logined')
+        const profile = JSON.parse(localStorage.getItem('user'))
+        const $avatar_img = document.querySelector('.avatar_img')
+        
+        is_logined.forEach(element => {
+            element.style.display = 'flex'
+        });
+        
+        if (profile.profileImage){
+            $avatar_img.src = 'http://127.0.0.1:8000'+ profile.profileImage
+        } else {
+            $avatar_img.src = '/src/assets/img/profile_temp.png'
+        }
+
+    } else {
+        const is_not_logined = document.querySelectorAll('.is_not_logined')
+
+        is_not_logined.forEach(element => {
+            element.style.display = 'flex'
+        });
+    }
+}
+
+const logout = (event) => {
+    event.preventDefault()
+    
+    localStorage.removeItem('user');
+    location.href = '/index.html'
+}
+
+is_logined()
+$logout_btn.addEventListener('click',logout)
