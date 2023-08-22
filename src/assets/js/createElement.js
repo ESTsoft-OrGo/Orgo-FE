353351d98e96fe362aea79a601683ba87a6373cd
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-08-22 20:37:51 +0900

    feat : Modify createElement.js

--- src/assets/js/createElement.js
+++ src/assets/js/createElement.js
@@ -1,4 +1,4 @@
-export const create_post = (post,owner) => {
+export const create_post = (post,owner,where,likes) => {
     const post_div = document.createElement('div');
     const post_owner = document.createElement('div');
     const post_owner_div = document.createElement('div');
@@ -6,6 +6,8 @@ export const create_post = (post,owner) => {
     const post_owner_info = document.createElement('div');
     const post_owner_info_p1 = document.createElement('p');
     const post_owner_info_p2 = document.createElement('p');
+    const post_owner_follow = document.createElement('div');
+    const post_owner_follow_btn = document.createElement('button');
     const post_content = document.createElement('a');
     const post_title = document.createElement('p');
     const post_contents = document.createElement('p');
@@ -19,12 +21,19 @@ export const create_post = (post,owner) => {
     const reaction_info_div2 = document.createElement('div');
     const reaction_info_div2_p1 = document.createElement('p');
     const reaction_info_div2_p2 = document.createElement('p');
-    
+
     post_div.className = 'post'
     post_owner.className = 'post_owner';
     post_owner_div.className = 'post_owner_img'
     if (owner.profileImage){
-        post_owner_img.src = 'http://127.0.0.1:8000'+ owner.profileImage
+        let media_url;
+        if (where == "board") {
+            media_url = 'http://127.0.0.1:8000/media/'
+        }
+        else if(where == "profile"){
+            media_url = 'http://127.0.0.1:8000'
+        }
+        post_owner_img.src = media_url + owner.profileImage
     } else {
         post_owner_img.src = '/src/assets/img/profile_temp.png'
     }
@@ -32,12 +41,18 @@ export const create_post = (post,owner) => {
     post_owner_info_p1.innerText = owner.nickname
     post_owner_info_p2.innerText = owner.about
 
-    post_owner.append(post_owner_div,post_owner_info)
+    post_owner_follow.className = 'post_owner_follow'
+    post_owner_follow_btn.id = owner.user_id
+    post_owner_follow_btn.innerText = 'Follow'
+
+    post_owner.append(post_owner_div,post_owner_info,post_owner_follow)
     post_owner_div.append(post_owner_img)
     post_owner_info.append(post_owner_info_p1,post_owner_info_p2)
+    post_owner_follow.append(post_owner_follow_btn)
 
     post_content.className = 'post_content'
-    post_content.href = '#'
+    post_content.id = post.id
+    post_content.href = '/src/view/view.html'
     post_title.className = 'post_title'
     post_title.innerText = post.title
     post_contents.innerText = post.content
@@ -47,15 +62,22 @@ export const create_post = (post,owner) => {
     post_img.className = 'post_img'
 
     post_createdat_div.className = 'post_createdat'
-    post_createdat.innerText = post.created_at
-
+    
+    const time = new Date(post.created_at)
+    const year = time.getFullYear();
+    const month = time.getMonth() +1;
+    const date = time.getDate();
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    
+    post_createdat.innerText = `${year}년 ${month}월 ${date}일 ${hours}시 ${minutes}분 `
     post_createdat_div.append(post_createdat)
 
     post_reaction_info.className = 'post_reaction_info'
     reaction_info_div1.className = 'reaction_info_div'
     reaction_info_div1_p1.innerText = '좋아요'
     reaction_info_div1_p2.className = 'like_count'
-    reaction_info_div1_p2.innerText = '0'
+    reaction_info_div1_p2.innerText = likes
     reaction_info_div2.className = 'reaction_info_div'
     reaction_info_div2_p1.innerText = '조회'
     reaction_info_div2_p2.className = 'view_count'
@@ -97,7 +119,8 @@ export const create_follow = (data) => {
     follow_info.append(follow_info_p1,follow_info_p2)
 
     follow_cancle.className = 'follow_cancle'
-    follow_cancle_btn.innerText = '팔로우'
+    follow_cancle_btn.id = data.user_id
+    follow_cancle_btn.innerText = 'Follow'
     follow_cancle.append(follow_cancle_btn)
 
     follow.append(follow_img_div,follow_info,follow_cancle)
