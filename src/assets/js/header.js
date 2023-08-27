b94d058a7048a8235325f7973641d6d4aaf7f239
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-08-27 15:42:43 +0900

    feat : Notfiy Function Apply All Pages

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -13,6 +13,15 @@ const is_logined = async() => {
         const profile = JSON.parse(getWithExpire('user'))
         const $avatar_img = document.querySelector('.avatar_img')
         
+        const notify = JSON.parse(localStorage.getItem('myNotify'))
+        const $notify_count = document.querySelector('.notify_count')
+        if(notify.length > 0) {
+            $notify_count.style.display = 'flex'
+            $notify_count.innerText = notify.length
+        } else {
+            $notify_count.style.display = 'none'
+        }
+
         is_logined.forEach(element => {
             element.style.display = 'flex'
         });
@@ -27,6 +36,15 @@ const is_logined = async() => {
         notisocket.onmessage = (e) => {
             const receiveData = JSON.parse(e.data)
             localStorage.setItem('myNotify', JSON.stringify(receiveData.message));
+            const newNotify = JSON.parse(localStorage.getItem('myNotify'))
+            const $notify_count = document.querySelector('.notify_count')
+
+            if(newNotify.length > 0) {
+                $notify_count.style.display = 'flex'
+                $notify_count.innerText = newNotify.length
+            } else {
+                $notify_count.style.display = 'none'
+            }
         }
 
     } else {
---
+++ src/assets/js/notify.js
@@ -0,0 +1,51 @@
+import { getCookie, detail_page, getWithExpire,setWithExpire} from "./util.js"
+import { create_notify } from "./createElement.js"
+import { followFunc } from "./follow.js"
+
+
+const myNotification = async () => {
+    const $notify_list = document.querySelector('.notify_list')
+
+    const url = 'http://127.0.0.1:8000/notify/'
+    const access = getCookie('access')
+
+    await fetch(url, {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${access}`,
+        },
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        if (data.notify){
+
+            const $notify_none = document.querySelector('.notify_none')
+
+            const notifications = data.notify
+            notifications.forEach(notification => {
+                const element = create_notify(notification)
+                $notify_list.append(element)
+            });
+
+            localStorage.setItem('myNotify', JSON.stringify(notifications));
+            const newNotify = JSON.parse(localStorage.getItem('myNotify'))
+            const $notify_count = document.querySelector('.notify_count')
+
+            if(newNotify.length > 0) {
+                $notify_none.remove()
+                $notify_count.style.display = 'flex'
+                $notify_count.innerText = newNotify.length
+            } else {
+                $notify_count.style.display = 'none'
+            }
+        }
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+    
+}
+
+myNotification()
+
