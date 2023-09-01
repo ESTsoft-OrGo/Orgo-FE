76be79b9795c5ef21763a82d6edd1917e7b4c07b
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-09-01 11:32:48 +0900

    feat : 검색 기능에 스터디 추가

--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -1,5 +1,5 @@
-import { detail_page,getWithExpire,slide_func } from "./util.js"
-import { create_post,create_follow } from "./createElement.js"
+import { detail_page,getWithExpire,slide_func,study_page } from "./util.js"
+import { create_post,create_follow,create_study } from "./createElement.js"
 import { followFunc } from "./follow.js"
 
 const $relatedBtn = document.querySelectorAll('.related');
@@ -21,14 +21,16 @@ const post_list = async () => {
     })
     .then((res) => res.json())
     .then((data) => {
-        console.log(data)
         const $post_list = document.querySelector('.post_list')
         const $profile_list = document.querySelector('.profile_list')
+        const $study_list = document.querySelector('.study_list')
         const $search_result = document.querySelector('.search-result > p')
         const $post_search = document.querySelector('.post_search')
         const $profile_search = document.querySelector('.profile_search')
+        const $study_search = document.querySelector('.study_search')
         const $post_none = document.querySelector('.post_none')
         const $profile_none = document.querySelector('.profile_none')
+        const $study_none = document.querySelector('.study_none')
         const posts = data.posts
         const profiles = data.profiles
         const studies = data.studies
@@ -37,6 +39,8 @@ const post_list = async () => {
 
         $post_search.innerText = `게시물 ${data.posts.length}건`
         $profile_search.innerText = `프로필 ${data.profiles.length}건`
+        $study_search.innerText = `스터디 ${data.studies.length}건`
+
 
         if(posts.length > 0){
             $post_none.remove()
@@ -56,6 +60,21 @@ const post_list = async () => {
                 $profile_list.append(element)
             });
         }
+
+        if(studies.length > 0){
+            $study_none.remove()
+            studies.forEach(element => {
+                const study = create_study(element)
+                $study_list.append(study)
+            });
+    
+            const $study_div = document.querySelectorAll('.study_div')
+    
+            $study_div.forEach(element => {
+                element.addEventListener('click',study_page)
+            });
+        }
+        
 
         const articles = document.querySelectorAll('.post_content')
 
@@ -100,15 +119,19 @@ const relatedClick = (e) => {
     const target = e.target
     const $choice_post = document.querySelector('.choice_post')
     const $choice_profile = document.querySelector('.choice_profile')
+    const $choice_study = document.querySelector('.choice_study')
 
     $choice_post.classList = 'choice_post hidden'
     $choice_profile.classList = 'choice_profile hidden'
+    $choice_study.classList = 'choice_study hidden'
 
     if(target.innerText.includes('게시물')){
         $choice_post.classList = 'choice_post'
     } else if(target.innerText.includes('프로필')){
         $choice_profile.classList = 'choice_profile'
-    } 
+    } else if(target.innerText.includes('스터디')){
+        $choice_study.classList = 'choice_study'
+    }
 
     $relatedBtn.forEach(element => {
         element.classList = 'related'
